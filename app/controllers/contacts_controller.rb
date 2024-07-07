884e2bbe65423f6075023f64cbd4c6e417a0c36a
ninapferrari <ninapferrari@gmail.com>
2024-07-07 15:42:42 -0300

    feat:create association user to many contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -65,6 +65,6 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :phone, :notes)
+      params.require(:contact).permit(:first_name, :last_name, :phone, :notes,  :user_id)
     end
 end
